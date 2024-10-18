18c0f4f8c107257d98b59b6c3cdb3006b2d880a1
rosstaylor95 <ross.taylor9500@gmail.com>
2024-10-18 22:43:53 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enable static export
+};
 
-export default nextConfig;
+module.exports = nextConfig;
